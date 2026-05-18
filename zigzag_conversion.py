25e9e2489926b5bd981f3d71d3ee45bf1b23497c
Suchitra-Santra <santrasuchitra07@gmail.com>
2026-05-18 21:20:41 +0530

    Added zigzag conversion solution

---
+++ zigzag_conversion.py
@@ -0,0 +1,33 @@
+class Solution:
+    def convert(self, s, numRows):
+
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [''] * numRows
+
+        currentRow = 0
+        goingDown = False
+
+        for char in s:
+            rows[currentRow] += char
+
+            if currentRow == 0 or currentRow == numRows - 1:
+                goingDown = not goingDown
+
+            if goingDown:
+                currentRow += 1
+            else:
+                currentRow -= 1
+
+        return ''.join(rows)
+
+
+# Driver code
+s = "PAYPALISHIRING"
+numRows = 3
+
+obj = Solution()
+result = obj.convert(s, numRows)
+
+print("Output:", result)
