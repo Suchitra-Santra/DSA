21fbe2a32558dbafaad551443fbe9bd33c452c0a
Suchitra-Santra <santrasuchitra07@gmail.com>
2026-08-29 20:47:54 +0530

    Initial commti

---
+++ Divby5and7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Divby5and7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num % 5 == 0 && num % 7 == 0) {
+            System.out.println(num + " is divisible by both 5 and 7");
+        } else {
+            System.out.println(num + " is not divisible by both 5 and 7");
+        }
+
+        sc.close();
+    }
+}
